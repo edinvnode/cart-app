a28cd82e2f48d7c19b3a6f176d8ab7925d6ba146
Edin <edin.vatrich@gmail.com>
2023-07-24 21:33:51 +0200

    First upload

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    title: 'Samsung Galaxy S7',
+    price: 599.99,
+    img: 'https://image-us.samsung.com/SamsungUS/home/mobile/phones/pdp/sm930/gallery/smg935-0hero-0930.jpg?$product-details-jpg$',
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: 'google pixel ',
+    price: 499.99,
+    img: 'https://www.android.com/intl/en_ca/static/2016/img/devices/phones/pixel/silver-1_w_1x.jpg',
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: 'Xiaomi Redmi Note 2',
+    price: 699.99,
+    img: 'https://p-ph.ipricegroup.com/uploaded_72a7c8010169136e7a48b93b8cc03eb1.jpg',
+    amount: 1,
+  },
+];
